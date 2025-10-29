fb304b4ac925176b78acd90fe89b4b65454e7245
Puffer <chipkarsaish162@gmail.com>
2025-10-30 00:36:59 +0530

    React

---
+++ Car.jsx
@@ -0,0 +1,9 @@
+function Car(props){
+    return(
+        <>
+        <h1>Hello {props.color}</h1>
+        </>
+    )
+}
+
+export default Car;
---
+++ Carm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Carm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <h2>I am a {this.props.model}!</h2>;
+  }
+}
+
+export default Carm;
